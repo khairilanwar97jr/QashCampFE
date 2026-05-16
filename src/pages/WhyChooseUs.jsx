ee9760b4121cc4aeab779f6345b3c51c4f697d88
khairil anwar github <khairilanwar97jr@gmail.com>
2026-05-16 23:53:25 +0800

    fix alias import for vercel build

--- src/pages/WhyChooseUs.jsx
+++ src/pages/WhyChooseUs.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "../components/ui/card";
 import { CheckCircle } from "lucide-react";
 import Navbar from "../components/Navbar";
 import maintenance1 from "../assets/maintenance1.jpg";
